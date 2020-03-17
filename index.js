626d835e957863daebba3126b885ee9d12198945
bassant abdelgawad <bassant.agawad@gmail.com>
2020-03-17 16:33:36 +0200

    watching for changes in project dir

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
 
+const debounce = require('lodash.debounce');
+// watch for changes with chokidar library
+const chokidar = require('chokidar');
+
+const start = debounce(() => {
+  console.log('STARTING USER\'S PROGRAM');
+}, 100)
+
+chokidar.watch('.')
+  .on('add', start)
+  .on('change', () => console.log('FILE CHANGED'))
+  .on('unlink', () => console.log('FILE UNLINKED')); // file is deleted
